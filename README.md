dd6749a69e00108a027030714839922a4f14742a
Nemanja Popovcevic <npopovcevic@gmail.com>
2023-11-30 14:57:18 +0100

    Uploaded readme and timeResttrictedAccsessMiddleware

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 You have to create .env file. It contains folowing strings:
-*DB_STRING=
-*SECRET=
-*ADMIN_USERNAME=
-*ADMIN_PASSWORD=
-*ipAdress=
-*port=
+-DB_STRING
+-SECRET
+-ADMIN_USERNAME
+-ADMIN_PASSWORD
+-ipAdress
+-port
+-timeRestrictStart
+-timeRestrictEnd
--- middleware/timeRestrictedAccessMiddleware.js
+++ middleware/timeRestrictedAccessMiddleware.js
@@ -5,7 +5,7 @@ module.exports.timeRestrictedAccsess = (req, res, next) => {
     const userRole = req.user.role;
 
     // Allow access only between 11 and 12 o'clock for "klijent" role
-    if (userRole === 'klijent' && currentTime >= 13 && currentTime < 14) {
+    if (userRole === 'klijent' && currentTime >= process.env.timeRestrictStart && currentTime < process.env.timeRestrictEnd) {
         req.flash('info', 'Unos i brisanje pošiljaka za današnji dan je završen. Započnite unos pošiljaka za sutra nakon 12h. Hvala na razumevanju!');
 
 
