78ada0504e40862e59227b2e4f7b1d010b378da8
Nemanja Popovcevic <npopovcevic@gmail.com>
2024-05-21 11:39:02 +0200

    New funcionality, download pdf for printing address label.

--- routes/paketRoutes.js
+++ routes/paketRoutes.js
@@ -4,11 +4,13 @@ const isAuth = require('../middleware/authMiddleware').isAuth;
 const isAdmin = require('../middleware/authMiddleware').isAdmin;
 const timeRestrictedAccsess = require('../middleware/timeRestrictedAccessMiddleware').timeRestrictedAccsess;
 const exportToExcel = require('../lib/exportToExcell');
+const downloadPdf = require('../lib/pdfPrinter');
 const router = express.Router();
 
 router.get('/', isAuth, paketController.paket_index);
 router.get('/admin', isAdmin, paketController.paket_admin);
 router.get('/export-to-excel', isAuth, exportToExcel);
+router.get('/downloadpdf', isAuth, downloadPdf);
 router.get('/deleteall', isAdmin, paketController.paket_delete_all);
 
 router.post('/', isAuth, timeRestrictedAccsess, paketController.paket_create_post);
