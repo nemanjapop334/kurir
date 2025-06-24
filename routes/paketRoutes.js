0ea7c69d086e612ab680eaae91377005b7b9430c
Nemanja Popovcevic <nemanja@Nemanjas-MacBook-Pro.local>
2025-06-24 19:29:06 +0200

    PDF printer2

--- routes/paketRoutes.js
+++ routes/paketRoutes.js
@@ -5,12 +5,14 @@ const isAdmin = require('../middleware/authMiddleware').isAdmin;
 const timeRestrictedAccsess = require('../middleware/timeRestrictedAccessMiddleware').timeRestrictedAccsess;
 const exportToExcel = require('../lib/exportToExcell');
 const downloadPdf = require('../lib/pdfPrinter');
+const downloadpdf2 = require('../lib/pdfPrinter2');
 const router = express.Router();
 
 router.get('/', isAuth, paketController.paket_index);
 router.get('/admin', isAdmin, paketController.paket_admin);
 router.get('/export-to-excel', isAuth, exportToExcel);
 router.get('/downloadpdf', isAuth, downloadPdf);
+router.get('/downloadpdf2', isAuth, downloadpdf2);
 router.get('/deleteall', isAdmin, paketController.paket_delete_all);
 
 router.post('/', isAuth, timeRestrictedAccsess, paketController.paket_create_post);
